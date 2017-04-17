7a521fef5d31c09555f7b0cab34214500e72144f
9495774207 <9495774207@pc142009m.ecc.u-tokyo.ac.jp>
2017-04-17 17:38:33 +0900

    date2

---
+++ biostatistics2017/data2/date2.R
@@ -0,0 +1,13 @@
+read.table("crops.txt", header=T) -> crop
+crop
+
+plot(crop)
+
+crop.dist <- dist(crop, "manhattan")
+crop.dist
+
+crop.clust <- hclust(crop.dist, "average")
+plot(crop.clust)
+
+crop.clust <- hclust(crop.dist, "average")
+plot(crop.clust, main="", sub"", xlab="", ylab="成分量の差")
